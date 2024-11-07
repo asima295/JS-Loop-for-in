6f656dfd390fd274efd3af8bea7e3cffc35de311
asima295 <ftasleem581@gmail.com>
2024-11-07 09:45:10 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,248 @@
+let x = 5;
+let y =7;
+let z = x+y;
+console.log(z)
+document.getElementById('p1').innerHTML=z;
+
+let a=7;
+let b=5;
+let c=a-b;
+console.log(c)
+document.getElementById('p2').innerHTML=c;
+
+let d=7;
+let e=5;
+let f=a*b;
+document.getElementById('p3').innerHTML=f;
+console.log(f)
+
+let g=7;
+let h=5;
+let i=a/b;
+document.getElementById('p4').innerHTML=i;
+console.log(i)
+
+
+let j=7;
+let k=5;
+let l=a%b;
+document.getElementById('p5').innerHTML=l;
+console.log(l)
+
+let m=7;
+let n=(10+20)*m;
+document.getElementById('p6').innerHTML=n;
+console.log(n)
+
+let o=7;
+let p=(100+50)/m;
+document.getElementById('p7').innerHTML=p;
+console.log(p)
+
+let r=7;
+r++;
+document.getElementById('p8').innerHTML=r;
+console.log(r)
+
+let s=7;
+s--;
+document.getElementById('p9').innerHTML=s;
+console.log(s)
+
+let t=10;
+let u= Math.pow(t,3);
+document.getElementById('p10').innerHTML=u;
+console.log(u)
+
+let v=10+5-3;
+document.getElementById('p11').innerHTML=v;
+console.log(v)
+
+let w=10**3;
+document.getElementById('p12').innerHTML=w;
+console.log(w)
+
+let AM=10*5/2;
+document.getElementById('p13').innerHTML=AM;
+console.log(AM)
+
+let AB=10;
+let AC=AB**2
+document.getElementById('p14').innerHTML=AC;
+console.log(AC)
+
+let t1 = "A";
+let t2 = "B";
+document.getElementById('p15').innerHTML=t1>t2;
+console.log(t1>t2)
+
+let t3 = "7";
+let t4 = "8";
+document.getElementById('p16').innerHTML=t3>t4;
+console.log(t3<t4)
+
+let t5 = "John";
+let t6 = "Doe";
+let t7 = t5 + " " + t6;
+document.getElementById('p17').innerHTML=t7;
+console.log(t7)
+
+
+let t8 = 9;
+let t9 = 8;
+let t10 = t9 + " " + t8;
+document.getElementById('p18').innerHTML=t10;
+console.log(t10)
+
+
+let a1 = 5 + 5;
+let a2= "5" +" "+ 5;
+let aa2= "5"+ 5;
+let aa3= "5"+"5";
+let aa4= "5"+" "+"5";
+let a3= "Hello" + 5;
+document.getElementById('p19').innerHTML=a1;
+console.log(a1)
+document.getElementById('p20').innerHTML=a2;
+console.log(a2)
+document.getElementById('p21').innerHTML=aa2;
+console.log(aa2)
+document.getElementById('p22').innerHTML=aa3;
+console.log(aa3)
+document.getElementById('p23').innerHTML=aa4;
+console.log(aa4)
+document.getElementById('p24').innerHTML=a3;
+console.log(a3)
+
+
+let aa = 10;
+aa -= 5;
+document.getElementById('p25').innerHTML=aa;
+console.log(aa)
+
+
+let bb = 10;
+bb *= 5;
+document.getElementById('p26').innerHTML=bb;
+console.log(bb)
+
+
+
+
+let dd = 10;
+dd /= 5;
+document.getElementById('p27').innerHTML=dd;
+console.log(dd)
+
+
+let ee = 106;
+ee %= 5;
+document.getElementById('p28').innerHTML=ee;
+console.log(ee)
+
+
+let dd1 = 10;
+let dd2= 5;
+let dd3=dd1+=dd2;
+document.getElementById('p30').innerHTML=dd3;
+console.log(dd3)
+
+
+
+
+
+let d4 = 10;
+let d5= 5;
+let d6=d4-=d5;
+document.getElementById('p31').innerHTML=d6;
+console.log(d6)
+
+
+let d7 = 10;
+let d8= 5;
+let d9=d7*=d8;
+document.getElementById('p32').innerHTML=d9;
+console.log(d9)
+
+
+let d10 = 12;
+let d11 = 5;
+let d12=d10/=d11;
+document.getElementById('p33').innerHTML=d12;
+console.log(d12)
+
+
+let d13 = 12;
+let d14 = 5;
+let d15=d13%=d14;
+document.getElementById('p34').innerHTML=d15;
+console.log(d15)
+
+
+let z1=10/2*5+2-1;
+document.getElementById('p35').innerHTML=z1;
+console.log(z1)
+
+
+let z2=10;
+let z3=10;
+let z4=z2==z3;
+document.getElementById('p36').innerHTML=z4;
+console.log(z4)
+
+
+let z5=10;
+let z6=10;
+let z7=z5===z6;
+document.getElementById('p37').innerHTML=z7;
+console.log(z7)
+
+
+let z8=11;
+let z9=10;
+let z10=z8==z9;
+document.getElementById('p38').innerHTML=z10;
+console.log(z10)
+
+
+let z11=11;
+let z12=10;
+let z13=z11===z12;
+document.getElementById('p39').innerHTML=z13;
+console.log(z13)
+
+
+let z14=10==11;
+document.getElementById('p40').innerHTML=z14;
+console.log(z14)
+
+
+let z15=10==10;
+document.getElementById('p41').innerHTML=z15;
+console.log(z15)
+
+let z16=10===10;
+document.getElementById('p42').innerHTML=z16;
+console.log(z16)
+
+
+let z17=10===11;
+document.getElementById('p43').innerHTML=z17;
+console.log(z17)
+
+
+let cc = 10;
+cc **= 5;
+document.getElementById('p44').innerHTML=cc;
+console.log(cc)
+
+
+let d1 = 10;
+let d2= 5;
+let d3=dd1**=dd2;
+document.getElementById('p45').innerHTML=d3;
+console.log(d3)
+
+
+
+console.log(typeof(Array))
